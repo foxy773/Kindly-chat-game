2c25041eb5dcc23fbf9b24b13a3fe97aefe9c788
foxy773 <felix_1999@live.no>
2022-07-06 15:27:44 +0200

    :construction: Add auto generating platforms

--- js/game.js
+++ js/game.js
@@ -5,14 +5,20 @@ window.onload = function () {
     startNewGame();
     setInterval(update, 10);
 }
-
+// Global Variables
 // Player / Character attributes
 
+let player;
+let gravity = 0.1;
+let obstacles = [];
+let obstacleY = 800;
+let level;
 
 const playerRadius = 20;
 const playerHeight = playerRadius;
 const playerWidth = playerRadius + 10;
 
+// Obstacle / Platform attributes
 const obstacleHeight = 10;
 const obstacleWidth = 60;
 
@@ -23,7 +29,7 @@ class Player {
         this.r = r;
         this.width = playerWidth;
         this.height = playerHeight;
-        this.ySpeed = 3;
+        this.ySpeed = 1;
         this.xSpeed = 0;
     }
 
@@ -38,6 +44,7 @@ class Player {
 
     update() {
         this.x += this.xSpeed;
+        console.log()
     }
 }
 
@@ -54,21 +61,17 @@ class Obstacle {
         this.chance = Math.floor(Math.random() * 10);
     }
     show() {
-        if (this.chance === 2) {
+        /* if (this.chance === 2) {
             this.moving = true;
-        }
+        } */
         if (this.visible) {
             c.fillStyle = 'red';
             c.fillRect(this.x, this.y, this.width, this.height);
-            /* if (this.hasSpring) {
-                c.fillStyle = 'gray';
-                c.fillRect(this.springX, this.y - 10, 10, 10);
-            } */
         }
     }
     update() {
         /* Removes the obstacles that are below the player and out of frame */
-        if (this.y > canvas.height +100) {
+        if (this.y > canvas.height + 150) {
             this.visible = false;
         }
 
@@ -78,52 +81,74 @@ class Obstacle {
 
         if (player.x < this.x + this.width && player.x + player.width > this.x && player.y < this.y + this.height && player.y + player.height > this.y && this.wasAbove && this.visible) {
             player.ySpeed = -800;
+            console.log(obstacleY);
+            console.log(player.ySpeed)
         }
 
-        let playerIsDead = obstacles.every(obstacle => obstacle.visible === false)
+        if (player.y < obstacles[obstacles.length - 90].y /* && level < 1 */) {
+            level++;
+            generateObstacles();
+            console.log("Generate new obstacles", this.ySpeed, "ySpeed")
+        }
 
-        if (playerIsDead === true) {
+        let playerIsDead = obstacles.every(obstacle => obstacle.y < this.y / 10);
+
+        /* if (playerIsDead) {
             gameOver();
-        }
+        } */
+
+        obstacles[2].width = 10000;
+        obstacles[2].x = 0;
 
         /* Increases the fall speed/velocity of the player*/
-        this.y -= player.ySpeed*0.01;
+        this.y -= player.ySpeed * 0.01;
         player.ySpeed += gravity;
     }
 }
 
-let player;
-let gravity = 0.1;
-
-let obstacles = []; 
-let obstacleY = 800;
-
 function startNewGame() {
+    obstacles = [];
+    level = 0;
     player = new Player(300, 300, playerRadius);
-
-    for (let i = 0; i < 100; i++) {
-        var ob = new Obstacle(Math.floor(Math.random() * 600), obstacleY);
-        obstacles.push(ob);
-        obstacleY -= 100;
-    }
-    
+    generateObstacles()
+    player.xSpeed = 0;
     console.log("NEW GAME!")
 }
 
-function update() {
-   //background
-   c.fillStyle = 'lightblue';
-   c.fillRect(0, 0, 600, 800);
-   
-   //player
-   //obstacles
-   for (let i = 0; i < obstacles.length; i++) {
-       obstacles[i].show();
-       obstacles[i].update();
-   }
+function generateObstacles() {
+    const numberOfObstacles = 100;
+    /* obstacles = []; */
+    for (let i = 0; i < numberOfObstacles; i++) {
+        let ob = new Obstacle(Math.floor(Math.random() * 600), obstacleY);
+        obstacles.push(ob);
+        console.log("gen")
+        if (level === 0) {
+            obstacleY -= 100
+        } else {
+            
+        }
 
-   player.show();
-   player.update();
+        console.log(obstacleY)
+    }
+}
+
+function update() {
+    //background
+    c.fillStyle = 'lightblue';
+    c.fillRect(0, 0, 600, 800);
+
+    //player
+    //obstacles
+    for (var i = 0; i < obstacles.length; i++) {
+        obstacles[i].show();
+        obstacles[i].update();
+    }
+
+    player.show();
+    player.update();
+
+    /* console.log(obstacles) */
+
 }
 
 function keyDown(e) {
@@ -144,9 +169,17 @@ function keyUp(e) {
     }
 }
 
+let gameEnded = false;
 function gameOver() {
     console.log("GAME OVER!")
-    /* startNewGame(); */
+    gameEnded = true;
+}
+
+function resetGlobalVariables() {
+    /* gravity = 0.1;
+    player.ySpeed = 3;
+    player.xSpeed = 0;
+    player.y = 1000; */
 }
 
 document.onkeydown = keyDown;
